bb4c3d765ebce9077e239ca6437600603cc15f6b
Antoniocpena <antoniocpena11@gmail.com>
2025-08-26 08:40:52 -0600

    Antonio

--- Grafica.java
+++ Grafica.java
@@ -1,3 +1,8 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartUtils;
+import org.jfree.data.category.DefaultCategoryDataset;
 public class Grafica {
     
 }
