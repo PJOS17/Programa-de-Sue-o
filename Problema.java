bab1d7ab0fc60ca553888e1bccb4f6b3ed91733e
Josasd10 <joseschumann07@gmail.com>
2025-09-03 16:20:49 -0600

    Problemas

--- Problema.java
+++ Problema.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problema {
-    String nomProblem;
+
+    
+    private List<String> problemaSueno;
+
+    public Problema(String problemaSueno) {
+        this.problemaSueno = new ArrayList<>();
+        this.problemaSueno.add(problemaSueno);
+    }
+    public static Problema newProblemaSuenoInsomnio() {
+        return new Problema("Insomnio");
+    }
+
+    public static Problema newProblemaSuenoPesadillas() {
+        return new Problema("Pesadillas");
+    }
+
+    public static Problema newProblemaSuenoApnea() {
+        return new Problema("Apnea de sueño");
+    }
+
+    public static Problema newProblemaSuenoNarcolepsia() {
+        return new Problema("Narcolepsia");
+    }
+
+    public List<String> getProblemaSueno() {
+        return problemaSueno;
+    }
+    public void agregarProblema(String problema) {
+        this.problemaSueno.add(problema);
+    }
 }
